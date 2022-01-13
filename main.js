8e07c1d00433f6842dc5f0a0dcf0dcbd2415842b
RenaIwata <puqpuqchan_07@icloud.com>
2022-01-13 16:02:34 +0900

    完成

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    
+    const valInput = document.getElementById('js-val-input');
+    const valKey = document.querySelectorAll('.js-val-key');
+    let textKey = "";
 
-
-    
+    for (let i = 0; i < valKey.length; i++) {
+        valInput.addEventListener('input', (e) => {
+            textKey = valKey[i].textContent;
+            if(textKey.includes(valInput.value)) {
+                valKey[i].closest('li').style.display = 'block';       
+            } else {
+                valKey[i].closest('li').style.display = 'none';  
+            }
+        });
+    }
 });
